2ff1673e10f759ad469aa5624ee36c39f8843583
Odina <149094996+turdievas@users.noreply.github.com>
2025-07-05 21:31:37 +0500

    Create lesson1hw.py

---
+++ lesson-1/homework/lesson1hw.py
@@ -0,0 +1,24 @@
+#1.
+side_of_square =12
+perimeter = 4*side_of_square
+area = side_of_square*side_of_square
+print (perimeter)
+print (area)
+#2.
+import math
+diameter = 20
+length_of_circle = math.pi*diameter
+print (length_of_circle)
+#3.
+a=10
+b=15
+mean=(a+b)/2
+print (mean)
+#4.
+a=2
+b=4
+sum=a+b
+product=a*b
+square_of_a=a**2
+square_of_b=b**2
+print(sum, product, square_of_a, square_of_b)
